4151b15d61cb0a178da47182d4cdb3ab1a332dcc
DEEPAK VARGHESE <deepak.29194@gmail.com>
2024-03-16 23:36:11 -0500

    create user update api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const signin = async (req,res,next) =>{
         const {password: pass, ...rest} = validUser._doc;
 
         res
-        .cookie('access token', token,{httpOnly : true })
+        .cookie('access_token', token,{httpOnly : true })
         .status(200)
         .json(rest);
         
@@ -62,7 +62,7 @@ export const google = async (req,res,next) =>{
             const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
             const {password:pass, ...rest} = user._doc;
             res
-            .cookie('access token', token,{httpOnly:true})
+            .cookie('access_token', token,{httpOnly:true})
             .status(200)
             .json(rest);
 
@@ -73,7 +73,7 @@ export const google = async (req,res,next) =>{
             await newUser.save();
             const token = jwt.sign({ id:newUser._id},process.env.JWT_SECRET);
             const {password:pass , ...rest} = newUser._doc;
-            res.cookie('access token',token,{httpOnly:true}).status(200).json(rest);
+            res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
         }
         
     } catch (error) {
